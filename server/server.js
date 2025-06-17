5d522053ff281400c12559d30d140e9e8035de40
unknown <46898377+mritun22@users.noreply.github.com>
2025-06-17 15:27:10 +0530

    changed server.js

--- server/server.js
+++ server/server.js
@@ -54,6 +54,7 @@ app.post("/get-recipe", (req, res) => {
 });
 
 
-app.listen(5000, () => {
-	console.log("Server started on port 5000...");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Server started on port ${port}...`);
 })
