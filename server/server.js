0ef6c5cc0c3a5a0001d18f76b7cdc1314542af91
unknown <46898377+mritun22@users.noreply.github.com>
2025-06-17 18:50:28 +0530

    changed server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,29 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Don't add triple backticks in the markdown.
 `;
 
+
+const hf = new InferenceClient(process.env.HF_ACCESS_TOKEN);
+
+async function getRecipeFromMistral(ingredientsArr) {
+    const ingredientsString = ingredientsArr.join(", ");
+
+    try {
+        const response = await hf.chatCompletion({
+            model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
+            messages: [
+                { role: "system", content: SYSTEM_PROMPT },
+                { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+            ],
+            max_tokens: 1024,
+        });
+
+        return response.choices[0].message.content;
+    } catch (err) {
+		console.log(err);
+        // console.error(err.message);
+    }
+}
+
 const anthropic = new Anthropic({
     apiKey: process.env.ANTHROPIC_API_KEY,
 })
@@ -49,7 +72,7 @@ app.post("/get-recipe", (req, res) => {
 	console.log(req.body);
 	// res.status(200).send(req.body);
 
-	getRecipeFromChefClaude(req.body.ingredientsArr)
+	getRecipeFromMistral(req.body.ingredientsArr)
 		.then(response => {
 			console.log(response)
 			res.status(200).send(response);
@@ -58,41 +81,21 @@ app.post("/get-recipe", (req, res) => {
 			console.error(err);
 			res.status(500).send("Internal Server Error");
 		});
+
+	
+	// getRecipeFromChefClaude(req.body.ingredientsArr)
+	// 	.then(response => {
+	// 		console.log(response)
+	// 		res.status(200).send(response);
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(err);
+	// 		res.status(500).send("Internal Server Error");
+	// 	});
 });
 
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server started on port ${port}...`);
-})
-
-
-
-
-
-
-// const hf = new InferenceClient(process.env.HF_ACCESS_TOKEN);
-
-// async function getRecipeFromMistral(ingredientsArr) {
-//     const ingredientsString = ingredientsArr.join(", ");
-
-//     try {
-//         const response = await hf.chatCompletion({
-//             model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-//             messages: [
-//                 { role: "system", content: SYSTEM_PROMPT },
-//                 { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
-//             ],
-//             max_tokens: 1024,
-//         });
-
-//         return response.choices[0].message.content;
-//     } catch (err) {
-// 		console.log(err);
-//         // console.error(err.message);
-//     }
-// }
-
-
-// getRecipeFromMistral(["mutton", "oil", "onion", "salt", "tomato"])
-// 	.then(response => console.log(response));
+});
